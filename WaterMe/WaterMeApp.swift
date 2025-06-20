bfd33bb9e5e47aa04466c15a8c2d98f872253a65
Santhosh Appan <santzapaul1990@gmail.com>
2025-06-20 18:57:31 -0400

    Initial Commit

---
+++ WaterMe/WaterMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterMeApp.swift
+//  WaterMe
+//
+//  Created by Santhosh Appan on 2025-06-20.
+//
+
+import SwiftUI
+
+@main
+struct WaterMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WaterMeTests/WaterMeTests.swift
@@ -0,0 +1,17 @@
+//
+//  WaterMeTests.swift
+//  WaterMeTests
+//
+//  Created by Santhosh Appan on 2025-06-20.
+//
+
+import Testing
+@testable import WaterMe
+
+struct WaterMeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
